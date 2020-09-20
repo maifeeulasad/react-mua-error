62d4d7b54a33f3e548feb859a2d723c9e1df658a
maifeeulasad <maifeeulasad@gmail.com>
2020-09-20 13:11:52 +0600

    //build for github.io

---
+++ precache-manifest.ab71fa0fb9050bf433833ac092d5e845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f87af52e0c82b7ed455b5938039dc2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06b34e3df5e2bb65d489",
+    "url": "./static/css/main.44887535.chunk.css"
+  },
+  {
+    "revision": "d1a52692a36482c0b688",
+    "url": "./static/js/2.ebc25e52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ebc25e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b34e3df5e2bb65d489",
+    "url": "./static/js/main.8ffa35dc.chunk.js"
+  },
+  {
+    "revision": "6fb151ac40499fbdae97",
+    "url": "./static/js/runtime-main.379e95e7.js"
+  }
+]);
